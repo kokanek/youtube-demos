abac9cc63947cef61fd88f8930935f60cce11a1c
kokaneka <kapeel.kokane@capillarytech.com>
2020-04-01 18:50:37 +0530

    refactoring with css file

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import { Typography } from 'antd';
 import './App.css';
+
 const { Title, Text } = Typography;
+
+const txt = {
+  joinCommunity: 'Join our community',
+  dayGuarrantee: '30-day, hassle free, money back guarrantee',
+  gainAccess: 'Gain access to our full library of tutorials along with expert code reviews.',
+  perfect: 'Perfect for any developer who are serius about honing their skills.',
+  monthlySub: 'Monthly Subscription',
+  dollar: '$29',
+  perMonth: 'per month',
+  fullAccess: 'Full access for less that $1 a day',
+  signUp: 'Sign Up',
+  whyUs: 'Why Us',
+  msgs: [
+    'Tutorials by industry experts',
+    'Peer & expert code review',
+    'Coding exercises',
+    'Access to our github repos',
+    'Community forum',
+    'Flashcards deck',
+    'New videos every week'
+  ]
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={styles.mainStyle}>
-          <div id="topDiv" style={styles.topDivStyle}>
-            <Title level={4} style={{color: '#3db5b0'}}>Join our community</Title>
-            <Text style={{ color: '#c5d674', fontSize: '18px', fontWeight: 'bold'}}>30-day, hassle free, money back guarrantee</Text>
-            <Text style={{ color: '#b2b6c1', fontSize: '18px', marginTop: '8px' }}>Gain access to our full library of tutorials along with expert code reviews.</Text>
-            <Text style={{ color: '#b2b6c1', fontSize: '18px' }}>Perfect for any developer who are serius about honing their skills.</Text>
+        <div className="main">
+          <div id="topDiv" className="topContainer">
+            <Title level={4} style={{color: '#3db5b0'}}>{txt.joinCommunity}</Title>
+            <Text style={{ color: '#c5d674', fontSize: '18px', fontWeight: 'bold' }}>{txt.dayGuarrantee}</Text>
+            <Text style={{ color: '#b2b6c1', fontSize: '18px', marginTop: '8px' }}>{txt.gainAccess}</Text>
+            <Text style={{ color: '#b2b6c1', fontSize: '18px' }}>{txt.perfect}</Text>
           </div>
-          <div id="bottomDiv" style={styles.bottomDivStyle}>
-            <div id='bottom-left-div' style={{ backgroundColor: '#2bb3b0', flex: '1 1', display: 'flex', padding: '48px', flexDirection: 'column', alignItems: 'flex-start', borderRadius: '0 0 0 12px'}}>
-              <Text style={{ color: '#efefef', fontSize: '18px', fontWeight: 'bold' }}>Monthly Subscription</Text>
-              <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                <Title level={2} style={{margin: '16px 0'}}>$29</Title><Text style={{color: '#ffffff77', fontSize: '16px', marginLeft: '8px'}}>per month</Text>
+          <div id="bottomDiv" className="bottomContainer">
+            <div id='bottom-left-div' className="bottomLeft">
+              <Text className="heading">{txt.monthlySub}</Text>
+              <div style={{display: 'flex', alignItems: 'center'}}>
+                <Title level={2} style={{margin: '16px 0'}}>{txt.dollar}</Title>
+                <Text style={{color: '#ffffff77', fontSize: '16px', marginLeft: '8px'}}>{txt.perMonth}</Text>
               </div>
-              <Text style={{ color: '#efefefdd', fontSize: '16px' }}>Full access for less that $1 a day</Text>
-              <button style={{ backgroundColor: '#BFDF32', color: '#efefef', fontSize: '16px', fontWeight: 'bold', padding: '12px', borderRadius: '4px', boxShadow: '2px 2px 10px 2px #77777777', width: '100%', marginTop: '24px' }}>Sign Up</button>
+              <Text style={{ color: '#efefefdd', fontSize: '16px' }}>{txt.fullAccess}</Text>
+              <button className="button">{txt.signUp}</button>
             </div>
-            <div id='bottom-right-div' style={{ backgroundColor: '#4CC1B9', flex: '1 1', display: 'flex', padding: '48px', flexDirection: 'column', alignItems: 'flex-start', borderRadius: '0 0 12px 0'}}>
-              <Text style={{ color: '#efefef', fontSize: '18px', fontWeight: 'bold' }}>Why Us</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '16px' }}>Tutorials by industry experts</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px'}}>Peer & expert code review</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>Coding exercises</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>Access to our github repos</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>Community forum</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>Flashcards deck</Text>
-              <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>New videos every week</Text>
+            <div id='bottom-right-div' className="bottomRight">
+              <Text className="heading" style={{ marginBottom: '12px' }}>{txt.whyUs}</Text>
+              {txt.msgs.map(msg => <Text style={{ color: '#ffffff99', fontSize: '16px', marginTop: '4px' }}>{msg}</Text>)}
             </div>
           </div>
         </div>
@@ -41,9 +59,6 @@ function App() {
 }
 
 const styles = {
-  mainStyle: { width: '700px', height: '500px', backgroundColor: '#eeeeee', borderRadius: '12px', display:'flex', flexDirection: 'column' },
-  topDivStyle: { padding: '18px 48px', display: 'flex', alignItems: 'flex-start', flexDirection: 'column', flex: '1 1', boxSizing: 'border-box' },
-  bottomDivStyle: { display: 'flex', flexDirection: 'row', flex: '1.4 1', borderRadius: '0 0 12px 12px' }
 }
 
 export default App;
